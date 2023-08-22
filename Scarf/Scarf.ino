5d9bd6700e1910ea0429a921eb62e6aee2b93bb3
Toshiro Goodson <tosh.goodson@gmail.com>
2023-08-22 13:47:07 -0700

    Update scarf for new LED strand

--- Scarf/Scarf.ino
+++ Scarf/Scarf.ino
@@ -4,8 +4,7 @@
 #endif
 
 #define PIN 1
-#define NUM_LEDS 23
-#define NEO_KHZ400 0x0100
+#define NUM_LEDS 29
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -15,7 +14,7 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_RGB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
